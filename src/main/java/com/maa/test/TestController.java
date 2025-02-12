cd1c582180e106dc08de8c3c59f4c766a3b3c12e
misho-999 <misho_angelov@yahoo.com>
2025-02-12 12:01:27 +0200

    Added @RequestMapping(/api)

--- src/main/java/com/maa/test/TestController.java
+++ src/main/java/com/maa/test/TestController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 //@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/api")
 public class TestController {
 
     @GetMapping("/test")
